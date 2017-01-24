35455964b4f1c6c7ac68afba8f56e9f2e6262be2
Roman Chehowski <r.chehowsky@gmail.com>
2017-01-25 00:27:32 +0300

    Initial commit!

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	[Serializable]
+	public struct NameForField
+	{
+		[SerializeField]
+		public string Name;
+
+		[SerializeField]
+		public string Field;
+	}
+
+	[SerializeField]
+	NameForField nameForField;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
